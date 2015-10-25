c5a9a0e6ad213460e65bb567fd9928689934b202
Cecilia C <xic.cecilia@gmail.com>
2015-10-25 00:48:43 -0400

    Update twoSum.cpp

--- twoSum.cpp
+++ twoSum.cpp
@@ -23,7 +23,6 @@ public:
 		// sort
 		vector<int> temp(nums.begin(), nums.end());
 		sort(temp.begin(), temp.end()); // O(nlogn)
-
 		// two pointers
 		int i = 0, j = nums.size() - 1;
 		while (i<j) {
@@ -31,15 +30,20 @@ public:
 			if (sum == target) break;
 			if (sum < target) ++i; else --j;
 		}
-
 		// find result
 		vector<int> ret;
 		int num1 = temp[i], num2 = temp[j];
 		for (int k = 0; k<nums.size(); k++) {
-			if (num1 == nums[k] || num2 == nums[k]) { ret.push_back(++k); break; }
+			if (num1 == nums[k] || num2 == nums[k]) {
+				ret.push_back(++k); 
+				break;
+			}
 		}
 		for (int k = ret[0]; k<nums.size(); k++) {
-			if (num1 == nums[k] || num2 == nums[k]) { ret.push_back(++k); break; }
+			if (num1 == nums[k] || num2 == nums[k]) { 
+				ret.push_back(++k); 
+				break;
+			}
 		}
 		return ret;
 	}
