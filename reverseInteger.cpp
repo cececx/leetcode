acb6e78b9f9d9114525495557fbc59735dc481a2
Cecilia C <xic.cecilia@gmail.com>
2015-10-25 06:31:43 -0400

    Update reverseInteger.cpp

--- reverseInteger.cpp
+++ reverseInteger.cpp
@@ -5,10 +5,8 @@
 /***********************************************************************
  *
  * Reverse digits of an integer.
- * 
  * Example1: x = 123, return 321
  * Example2: x = -123, return -321
- *
  * Assume that your function returns 0 when the reversed integer overflows.
  *
  **********************************************************************/
