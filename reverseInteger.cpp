291114a2ad9f71b7c5b231af0236e85d4de6090e
Cecilia C <xic.cecilia@gmail.com>
2015-10-25 06:31:31 -0400

    Update reverseInteger.cpp

--- reverseInteger.cpp
+++ reverseInteger.cpp
@@ -9,11 +9,7 @@
  * Example1: x = 123, return 321
  * Example2: x = -123, return -321
  *
- * Hint:
- * If the integer's last digit is 0, what should the output be? ie, cases such as 10, 100.
- * Did you notice that the reversed integer might overflow? 
- * Assume the input is a 32-bit integer, then the reverse of 1000000003 overflows. How should you handle such cases?
- * For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+ * Assume that your function returns 0 when the reversed integer overflows.
  *
  **********************************************************************/
 
