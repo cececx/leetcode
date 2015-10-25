135398dce452b694121feab0f48bac4170b75595
Cecilia C <xic.cecilia@gmail.com>
2015-10-25 00:47:15 -0400

    Update longestSubstringWithoutRepeat.cpp

--- longestSubstringWithoutRepeat.cpp
+++ longestSubstringWithoutRepeat.cpp
@@ -15,12 +15,10 @@ public:
 	// O(n), 12ms
 	// store s.size() and (int)s[i] ahead. runtime: 18ms -> 12ms
 	int lengthOfLongestSubstring(string s) {
-
 		int offset(0), len(0);
 		int pos[128];
 		memset(pos, -1, sizeof(int) * 128);
 		int size = s.size();
-		
 		for (int i; i < size; i++) {
 			int ch = s[i];
 			if (pos[ch] != -1) {
@@ -29,7 +27,6 @@ public:
 			}
 			pos[ch] = i;
 		}
-		
 		len = max(len, size - offset);
 		return len;
 	}
