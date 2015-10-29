ec32c09945d962b6a391a787c2966cfdee2850ac
Cecilia C <xic.cecilia@gmail.com>
2015-10-29 05:04:01 -0400

    28ms fast solution / 40ms solution in 2 lines

--- 012_integerToRoman.cpp
+++ 012_integerToRoman.cpp
@@ -33,9 +33,9 @@ public:
 	// 40ms. Two lines solution for fun :D
 	string intToRoman(int num) {
 		string p[34] = {"", "M", "MM", "MMM",
-						"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM",
-						"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC",
-						"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+		                "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM",
+		                "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC",
+		                "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
 		return p[num/1000] + p[num%1000/100+4] + p[num%100/10+14] + p[num%10+24];
 	}
 };
