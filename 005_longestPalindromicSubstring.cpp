9a33260e9d43e9c315483a2febd3873e5b896ba6
Cecilia C <xic.cecilia@gmail.com>
2015-10-29 00:39:20 -0400

    Two solutions: 4ms / 8ms

--- 005_longestPalindromicSubstring.cpp
+++ 005_longestPalindromicSubstring.cpp
@@ -12,31 +12,43 @@
  
 class Solution {
 public:
-	// O(n), 8ms 
-	string longestPalindrome(string s) {
-		// Initialize s[]->T[]
+	// 8ms. O(n). Manacher Algorithm
+	string longestPalindrome1(string s) {
 		string T = s;
 		for(int i=s.size(); i>=0; i--)
 			T.insert(i, 1, '#');
 		T.insert(0, 1, '>');
 		T += '<';
 		int P[2001] = {0};
-		// Manacher
-		int c(0), r(0), index(0), len(0);
+		int c(0), r(0), max(0);
 		for(int i = 1; i < T.size() - 1; i++){
-			if(r > i) 
-				P[i] = min(P[2 * c - i], r - i);
-			while(T[i+P[i]+1] == T[i-P[i]-1])
-				++P[i];
-			if(P[i] + i > r) { 
-				c = i; 
-				r = P[i] + i; 
+			if(r > i) P[i] = min(P[2 * c - i], r - i);	// Mirror
+			while(T[i+P[i]+1] == T[i-P[i]-1]) ++P[i];	// Expand
+			if(P[i] + i > r) { c = i;  r = P[i] + i; }	// Update current center index
+			if(P[i] > P[max]) max = i;
+		}
+		return s.substr( (max-P[max]-1)/2, P[max] );
+	}
+	
+	// 4ms. Is it O(nlogn)?
+	string longestPalindrome2(string s) {
+		if (s.size() < 2) return s;
+		int begin(0), len(1);
+		for (int i = 0; i < s.size();) {
+			if (s.size() - i <= len / 2) break;
+			int left = i, right = i;
+			while (right < s.size()-1 && s[right+1] == s[right]) ++right;	// Skip duplicate characters -> for even substring
+			i = right + 1;
+			while (right<s.size()-1 && left>0 && s[right+1] == s[left-1]) {	// Expand
+				++right; 
+				--left; 
 			}
-			if(P[i] > len) {
-				len = P[i];
-				index = i;
+			int newLen = right-left+1;  // pre-store (right-left+1)
+			if (len < newLen) { 		// >> runtime: 8ms->4ms  【贱人就是矫情(..•˘_˘•..)
+				begin = left; 
+				len = newLen;
 			}
 		}
-		return s.substr( (index-len-1)/2, len );
+		return s.substr(begin, len);
 	}
 };
