a61118ce0433f8c00798d60abf47101191c6294d
Cecilia C <xic.cecilia@gmail.com>
2015-10-28 23:53:18 -0400

    Longest Palindromic Substring

--- 005_longestPalindromicSubstring.cpp
+++ 005_longestPalindromicSubstring.cpp
@@ -12,33 +12,31 @@
  
 class Solution {
 public:
-    // O(n), 8ms 
-    string longestPalindrome(string s) {
-		
-         // Initialize s[]->T[]
-        string T = s;
-        for(int i=s.size(); i>=0; i--)
-            T.insert(i, 1, '#');
-        T.insert(0, 1, '>');
-	    T += '<';
-	    int P[2001] = {0};
-	    
-	    // Manacher
-	    int c(0), r(0), index(0), len(0);
-	    for(int i = 1; i < T.size() - 1; i++){
-	        if(r > i) 
-	            P[i] = min(P[2 * c - i], r - i);
-	        while(T[i+P[i]+1] == T[i-P[i]-1])
-	            ++P[i];
-	        if(P[i] + i > r) { 
-	            c = i; 
-	            r = P[i] + i; 
-	        }
-	        if(P[i] > len) {
-	            len = P[i];
-	            index = i;
-	        }
-	    }
-	    return s.substr( (index-len-1)/2, len );
-    }
+	// O(n), 8ms 
+	string longestPalindrome(string s) {
+		// Initialize s[]->T[]
+		string T = s;
+		for(int i=s.size(); i>=0; i--)
+			T.insert(i, 1, '#');
+		T.insert(0, 1, '>');
+		T += '<';
+		int P[2001] = {0};
+		// Manacher
+		int c(0), r(0), index(0), len(0);
+		for(int i = 1; i < T.size() - 1; i++){
+			if(r > i) 
+				P[i] = min(P[2 * c - i], r - i);
+			while(T[i+P[i]+1] == T[i-P[i]-1])
+				++P[i];
+			if(P[i] + i > r) { 
+				c = i; 
+				r = P[i] + i; 
+			}
+			if(P[i] > len) {
+				len = P[i];
+				index = i;
+			}
+		}
+		return s.substr( (index-len-1)/2, len );
+	}
 };
