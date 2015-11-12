69dbaa74d8c560f71e67b916a62223a58ef1c560
Cecilia <xic.cecilia@gmail.com>
2015-11-12 17:21:34 -0500

    Regular Expression Match: 4ms DP / 100ms BF

--- 010_regularExpressionMatch.cpp
+++ 010_regularExpressionMatch.cpp
@@ -10,22 +10,14 @@
  * '*' Matches zero or more of the preceding element.
  * 
  * The matching should cover the entire input string (not partial).
- * 
- * Some examples:
- * isMatch("aa","a") → false
- * isMatch("aa","aa") → true
- * isMatch("aaa","aa") → false
- * isMatch("aa", "a*") → true
- * isMatch("aa", ".*") → true
- * isMatch("ab", ".*") → true
- * isMatch("aab", "c*a*b") → true
  *
  **********************************************************************/
  
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        return DP(s, p);
+        // isMatchBF (s, p, 0, 0);
+        return isMatchDP (s, p);
     }
     
     // 4ms, DP
