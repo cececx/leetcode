f6241edb72f10809abf720eba9dae84655e50258
Cecilia <xic.cecilia@gmail.com>
2015-11-15 01:45:29 +0800

    16ms, O(n), using hash map: 2Sum

--- 001_twoSum.cpp
+++ 001_twoSum.cpp
@@ -18,33 +18,14 @@
 
 class Solution {
 public:
-	// 12ms
-	vector<int> twoSum(vector<int>& nums, int target) {
-		// sort
-		vector<int> temp(nums.begin(), nums.end());
-		sort(temp.begin(), temp.end()); // O(nlogn)
-		// two pointers
-		int i = 0, j = nums.size() - 1;
-		while (i<j) {
-			int sum = temp[i] + temp[j];
-			if (sum == target) break;
-			if (sum < target) ++i; else --j;
-		}
-		// find result
-		vector<int> ret;
-		int num1 = temp[i], num2 = temp[j];
-		for (int k = 0; k<nums.size(); k++) {
-			if (num1 == nums[k] || num2 == nums[k]) {
-				ret.push_back(++k); 
-				break;
-			}
-		}
-		for (int k = ret[0]; k<nums.size(); k++) {
-			if (num1 == nums[k] || num2 == nums[k]) { 
-				ret.push_back(++k); 
-				break;
-			}
-		}
-		return ret;
-	}
+    // 16ms, Time Complexity O(n), Space Complexity O(n)
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hash;
+        for(int i=0; i<nums.size(); ++i) {
+            if(hash.find( target-nums[i] ) != hash.end())
+                return {hash[ target-nums[i] ] + 1, i + 1};
+            hash[ nums[i] ] = i;
+        }
+        return {-1, -1};
+    }
 };
